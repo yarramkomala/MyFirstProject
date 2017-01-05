f987f03ab9eefe515c0c1d38647554ca408443f0
yarramkomala <yarramkomali@gmail.com>
2017-01-05 09:19:15 +0530

    4-1project

---
+++ shoppingcart/ShoppingCartApplication/src/main/java/com/niit/domain/UserInfo.java
@@ -0,0 +1,73 @@
+package com.niit.domain;
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="register")
+public class UserInfo  {
+	
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String username;
+	
+	private String password;
+	
+	private String fname;
+	
+	private String lname;
+	
+	private String address;
+	private String contactnumber;
+	
+	
+	public UserInfo(){}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactnumber() {
+		return contactnumber;
+	}
+	public void setContactnumber(String contactnumber) {
+		this.contactnumber = contactnumber;
+	}
+	
+	
+
+}
